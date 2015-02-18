78ac387dceaeade81fdb997eea61ef7e3333410f
addsthetics <addsthetics@gmail.com>
2015-02-18 02:55:58 -0800

    First attempt at solving 1.6.

---
+++ data_structures/arrays_and_strings/1_6/1_6_first_attempt.py
@@ -0,0 +1,39 @@
+def rotate_pixels(img_matrix):
+    res = []
+    num_rows = len(img_matrix)
+    num_cols = len(img_matrix[0])
+    for i in range(num_rows):
+        new_row = []
+        for j in range(num_cols):
+            new_row.append(img_matrix[num_cols - j - 1][i])
+        print(new_row)
+        res.append(new_row)
+    return res
+
+
+def main():
+    # print
+    # [7, 4, 1]
+    # [8, 5, 2]
+    # [9, 6, 3]
+    rotate_pixels([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+
+    print(rotate_in_place([
+        [2, 3, 4, 5],
+        [3, 4, 5, 6],
+        [4, 5, 6, 7],
+        [5, 6, 7, 8]
+    ]) == [
+        [5, 4, 3, 2],
+        [6, 5, 4, 3],
+        [7, 6, 5, 4],
+        [8, 7, 6, 5]
+    ])
+
+
+if __name__ == "__main__":
+    main()
